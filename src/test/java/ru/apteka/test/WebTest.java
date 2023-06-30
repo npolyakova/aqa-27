26d609681ff55e15b717962f7e192f710d83bc4c
n.polyakova <natli.d26.l8@gmail.com>
2023-06-30 14:28:48 +0300

    Check env

--- src/test/java/ru/apteka/test/WebTest.java
+++ src/test/java/ru/apteka/test/WebTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.Objects;
 
 import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
@@ -15,8 +16,8 @@ public class WebTest {
 
     @BeforeAll
     public static void setDriver() throws MalformedURLException {
-        System.out.println(System.getenv("IS_REMOTE"));
-        if (Boolean.getBoolean(System.getenv("IS_REMOTE"))) {
+        String isRemote = System.getenv("IS_REMOTE");
+        if (Objects.equals(isRemote, "true")) {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName("chrome");
             capabilities.setCapability("enableVNC:", true);
